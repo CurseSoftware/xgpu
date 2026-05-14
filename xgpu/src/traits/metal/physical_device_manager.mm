b6a27a0f8df0f7c1f8b8161e15461a8d2cf50d09
raustin9 <r.alex.austinfam@gmail.com>
2026-05-14 00:17:50 -0400

    device handling on mtl

--- xgpu/src/traits/metal/physical_device_manager.mm
+++ xgpu/src/traits/metal/physical_device_manager.mm
@@ -14,4 +14,9 @@ namespace xgpu::traits
     {
         return m_physical_devices[0];
     }
+
+    std::vector<data::PhysicalDevice>
+    PhysicalDeviceManager<core::GraphicsApi::Metal>::retrieve_physical_devices() noexcept
+    {
+    }
 } // namespace xgpu::traits
